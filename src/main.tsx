d8718a486fc58808a0c974803fdb7abcffc6f870
派大星的海洋裤 <15081150825@163.com>
2026-09-15 09:42:43 +0800

    refactor: 清理旧样式并统一基础组件样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles.css'
 import './design-system.css'
 import './components/workspace.css'
 
